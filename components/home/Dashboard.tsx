23f93802bf80ea943996ce76e55d3d10c2299c75
iamhectorsosa <hello@hectorsosa.me>
2024-05-31 18:42:36 +0200

    Updating

--- components/home/Dashboard.tsx
+++ components/home/Dashboard.tsx
@@ -2,11 +2,26 @@
 
 import React, { FC } from "react"
 import { useQuery } from "@tanstack/react-query"
+import * as z from "zod"
 
 import { createClient } from "@/modules/utils/client"
 import { getProfile } from "@/modules/user/profile"
 
 import { DashboardCard } from "./DashboardCard"
+
+type OnlineUser = {
+  username: string
+  presence_ref?: string
+}
+
+const onlineUserSchema = z.array(
+  z.object({
+    username: z.string(),
+    presence_ref: z.string().optional(),
+  })
+)
+
+const LOBBY_ROOM = "lobby-room"
 
 export const Dashboard: FC<{ userId: string }> = ({ userId }) => {
   const profile = useQuery({
@@ -14,48 +29,64 @@ export const Dashboard: FC<{ userId: string }> = ({ userId }) => {
     queryFn: () => getProfile({ id: userId }),
   })
 
-  const [state, setState] = React.useState("Hello")
+  const [users, setUsers] = React.useState<OnlineUser[]>([])
 
   const supabase = createClient()
 
   React.useEffect(() => {
-    const room = supabase.channel("test-room")
+    const room = supabase.channel(LOBBY_ROOM)
 
     room
       .on("presence", { event: "join" }, ({ newPresences }) => {
-        setState(JSON.stringify(newPresences))
+        const users = onlineUserSchema.safeParse(newPresences)
+        if (users.success) {
+          setUsers(users.data)
+        }
       })
       .on("presence", { event: "leave" }, ({ leftPresences }) => {
-        setState(JSON.stringify(leftPresences))
+        const users = onlineUserSchema.safeParse(leftPresences)
+        if (users.success) {
+          setUsers(users.data)
+        }
       })
-      .on("presence", { event: "sync" }, () => {
-        const newState = room.presenceState()
-        setState(JSON.stringify(newState))
-      })
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
       .subscribe(async (status) => {
         if (status !== "SUBSCRIBED") {
           return
         }
 
-        await room.track({
-          name: "Hector",
-          result: 0,
-          ready: true,
-        })
+        if (!profile.data || "error" in profile.data) return null
+
+        const user: OnlineUser = {
+          username: profile.data.username,
+        }
+        await room.track(user)
       })
 
     return () => {
       void supabase.removeChannel(room)
     }
-  }, [supabase])
+  }, [profile.data, supabase])
 
   if (!profile.data || "error" in profile.data) return null
 
   return (
-    <div>
-      <h1>Hello {profile.data.username}!</h1>
-      <p>{state}</p>
-      <DashboardCard />
+    <div className="mx-auto flex max-w-lg items-center justify-center py-12">
+      <div className="w-full space-y-3">
+        <h1>Hello, {profile.data.username}!</h1>
+        <div className="flex gap-x-2">
+          <DashboardCard winCount={0} matchesCount={1} />
+          <DashboardCard winCount={0} matchesCount={1} />
+          <DashboardCard winCount={0} matchesCount={1} />
+        </div>
+        <div>
+          <ul>
+            {users.map((user, i) => (
+              <li key={i}>{user.username}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
--- components/home/DashboardCard.tsx
+++ components/home/DashboardCard.tsx
@@ -1,17 +1,21 @@
-import { DollarSign } from "lucide-react"
+import * as React from "react"
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
-export function DashboardCard() {
+export const DashboardCard: React.FC<{
+  winCount: number
+  matchesCount: number
+}> = ({ winCount, matchesCount }) => {
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
-        <DollarSign className="size-4 text-muted-foreground" />
+        <CardTitle className="text-sm font-medium">Total Wins</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="text-2xl font-bold">$45,231.89</div>
-        <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+        <div className="text-2xl font-bold">{winCount}</div>
+        <p className="text-xs text-muted-foreground">
+          {winCount / matchesCount}% of matches
+        </p>
       </CardContent>
     </Card>
   )
